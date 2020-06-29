4a72417e690e7a82b6c478328892440f6804eb12
Seth Purr <eric.purrington@gmail.com>
2020-06-28 18:03:53 -0700

    some route ideas and file paths fixed

---
+++ routes/html-routes.js
@@ -0,0 +1,51 @@
+"/"
+"/stats"
+
+
+var path = require("path");
+
+module.exports = function(app) {
+  app.get("/", (req, res) => {
+    db.Note.find({})
+      .then(dbNote => {
+        res.json(dbNote);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+  app.get("/stats", (req, res) => {
+    db.User.find({})
+      .then(dbUser => {
+        res.json(dbUser);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+  app.post("/exercise", ({ body }, res) => {
+    db.Note.create(body)
+      .then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true }))
+      .then(dbUser => {
+        res.json(dbUser);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+  app.get("/populateduser", (req, res) => {
+    db.User.find({})
+      .populate("notes")
+      .then(dbUser => {
+        res.json(dbUser);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+};
+
--- server.js
+++ server.js
@@ -5,9 +5,13 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+const databaseUrl = "fitnessMonitor";
+const collections = ["exercises"];
+
+const db = mongojs(databaseUrl, collections);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
@@ -15,6 +19,10 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { 
 app.use(require("./routes/html-routes.js"));
 app.use(require("./routes/api-routes.js"));
 
+db.on("error", error => {
+    console.log("Database Error:", error);
+});
+  
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
