bee7b4a00a02864d42769cc80b2871ab6d096e9c
Seth Purr <eric.purrington@gmail.com>
2020-06-30 14:36:14 -0700

    properly working server set up

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
-const databaseUrl = "fitnessMonitor";
-const collections = ["exercises"];
-
-const db = mongojs(databaseUrl, collections);
-
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -18,10 +14,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { 
 
 app.use(require("./routes/html-routes.js"));
 app.use(require("./routes/api-routes.js"));
-
-db.on("error", error => {
-    console.log("Database Error:", error);
-});
   
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
